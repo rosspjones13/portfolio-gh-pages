cace637f1ea8ef24aad4e3128bb10f7d7174374f
Ross Jones <rossmosis.jones@gmail.com>
2019-08-11 10:14:58 -0400

    Updates

---
+++ precache-manifest.4f1fe6f3e93434cc15a89efb21bc31b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b5fc31486d4f26b1c1931046483530",
+    "url": "/portfolio-gh-pages/index.html"
+  },
+  {
+    "revision": "2c377c87ff231de905b9",
+    "url": "/portfolio-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "13dd5233ecb6ddb48d9e",
+    "url": "/portfolio-gh-pages/static/js/2.2ff4e546.chunk.js"
+  },
+  {
+    "revision": "2c377c87ff231de905b9",
+    "url": "/portfolio-gh-pages/static/js/main.3b0f47d0.chunk.js"
+  },
+  {
+    "revision": "919003e7e5db41033ead",
+    "url": "/portfolio-gh-pages/static/js/runtime~main.b75e6969.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
